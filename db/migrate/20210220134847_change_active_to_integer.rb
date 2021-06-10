8427e1c329d044af4e805a78c65cd0d571437d91
James Hall <jdhall@live.co.uk>
2021-06-10 05:52:49 +0100

    changed the columns from bool to int

--- db/migrate/20210220134847_change_active_to_integer.rb
+++ db/migrate/20210220134847_change_active_to_integer.rb
@@ -2,7 +2,7 @@
 
 class ChangeActiveToInteger < ActiveRecord::Migration[6.0]
   def change
-    change_column :games, :active, :integer, using: 'active::integer', default: 0
+    change_column :games, :active, :integer, using: 'active::integer', default: nil
     # Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
--- db/migrate/20210220180434_change_game_won_column_to_integer.rb
+++ db/migrate/20210220180434_change_game_won_column_to_integer.rb
@@ -2,7 +2,7 @@
 
 class ChangeGameWonColumnToInteger < ActiveRecord::Migration[6.0]
   def change
-    change_column :games, :game_won, :integer, default: 0
+    change_column :games, :game_won, :integer, using: 'game_won::integer', default: nil
     # Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
