494d3ecbf9ec998d59e0749f290dc9eac0f2c5e4
James Hall <jdhall@live.co.uk>
2021-06-10 04:24:52 +0100

    updated to postgres

--- db/migrate/20201225093104_add_default_value_to_mark.rb
+++ db/migrate/20201225093104_add_default_value_to_mark.rb
@@ -2,7 +2,7 @@
 
 class AddDefaultValueToMark < ActiveRecord::Migration[6.0]
   def change
-    change_column :cells, :mark, :integer, default: 0
+    change_column :cells, :mark, :integer, using: 'mark::integer', default: 0
     # Ex:- change_column("admin_users", "email", :string, :limit =>25)
   end
 end
--- db/migrate/20210102090942_rename_column_users_in_game_room_users.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RenameColumnUsersInGameRoomUsers < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :game_room_users, :users_id, :user_id
-    # Ex:- rename_column("admin_users", "pasword","hashed_pasword")
-  end
-end
